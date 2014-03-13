0986b32c850b74372eb44efb50c7a406a77d4430
lindseywells <Batcave@Batcave.local>
2014-03-12 22:29:29 -0700

    CSS/Scripts
    Added css styles
    Added form.
    Made form work.
    Chose bg photo.
    Changed scripts.

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,28 @@
 $(document).foundation();
 
 // Your Awesome Scripts!
+
+//$(document).ready(function() {
+  //navigator.geolocation.getCurrentPosition(function(position) {
+    //getWeather(position.coords.latitude+','+position.coords.longitude); //load weather using your lat/lng coordinates
+ // });
+// });
+
 $(document).ready(function() {
+  getWeather("yes"); //@params location, woeid
+});
+
+function getWeather(f) {
+  if (f==null) {
+  var loc=document.forms["zip"]["zipcode"].value;
+  } else{var loc="Spokane"}
   $.simpleWeather({
-    location: 'Austin, TX',
+    location: loc,
     woeid: '',
     unit: 'f',
     success: function(weather) {
       html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
       html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
-      html += '<li class="currently">'+weather.currently+'</li>';
       html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
   
       $("#weather").html(html);
@@ -19,6 +32,197 @@ $(document).ready(function() {
       $("#weather").html('<p>'+error+'</p>');
     }
   });
-});
+return false;
+}
 
-(function(e){"use strict";e.extend({simpleWeather:function(t){t=e.extend({location:"",woeid:"2357536",unit:"f",success:function(e){},error:function(e){}},t);var n=new Date;var r="//query.yahooapis.com/v1/public/yql?format=json&rnd="+n.getFullYear()+n.getMonth()+n.getDay()+n.getHours()+"&diagnostics=true&callback=?&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&q=";if(t.location!==""){r+='select * from weather.forecast where woeid in (select woeid from geo.placefinder where text="'+t.location+'" and gflags="R") and u="'+t.unit+'"'}else if(t.woeid!==""){r+="select * from weather.forecast where woeid="+t.woeid+' and u="'+t.unit+'"'}else{t.error("Could not retrieve weather due to an invalid location.");return false}e.getJSON(encodeURI(r),function(n){if(n!==null&&n.query.results!==null&&n.query.results.channel.description!=="Yahoo! Weather Error"){e.each(n.query.results,function(e,n){if(n.constructor.toString().indexOf("Array")!==-1){n=n[0]}var r=["N","NNE","NE","ENE","E","ESE","SE","SSE","S","SSW","SW","WSW","W","WNW","NW","NNW","N"];var i=r[Math.round(n.wind.direction/22.5)];if(n.item.condition.temp<80&&n.atmosphere.humidity<40){var s=-42.379+2.04901523*n.item.condition.temp+10.14333127*n.atmosphere.humidity-.22475541*n.item.condition.temp*n.atmosphere.humidity-6.83783*Math.pow(10,-3)*Math.pow(n.item.condition.temp,2)-5.481717*Math.pow(10,-2)*Math.pow(n.atmosphere.humidity,2)+1.22874*Math.pow(10,-3)*Math.pow(n.item.condition.temp,2)*n.atmosphere.humidity+8.5282*Math.pow(10,-4)*n.item.condition.temp*Math.pow(n.atmosphere.humidity,2)-1.99*Math.pow(10,-6)*Math.pow(n.item.condition.temp,2)*Math.pow(n.atmosphere.humidity,2)}else{var s=n.item.condition.temp}if(t.unit==="f"){var o="c";var u=Math.round(5/9*(n.item.condition.temp-32));var a=Math.round(5/9*(n.item.forecast[0].high-32));var f=Math.round(5/9*(n.item.forecast[0].low-32));var l=Math.round(5/9*(n.item.forecast[1].high-32));var c=Math.round(5/9*(n.item.forecast[1].low-32));var h=Math.round(5/9*(n.item.forecast[1].high-32));var p=Math.round(5/9*(n.item.forecast[1].low-32));var d=Math.round(5/9*(n.item.forecast[2].high-32));var v=Math.round(5/9*(n.item.forecast[2].low-32));var m=Math.round(5/9*(n.item.forecast[3].high-32));var g=Math.round(5/9*(n.item.forecast[3].low-32));var y=Math.round(5/9*(n.item.forecast[4].high-32));var b=Math.round(5/9*(n.item.forecast[4].low-32))}else{var o="f";var u=Math.round(9/5*n.item.condition.temp+32);var a=Math.round(9/5*n.item.forecast[0].high+32);var f=Math.round(9/5*n.item.forecast[0].low+32);var l=Math.round(9/5*(n.item.forecast[1].high+32));var c=Math.round(9/5*(n.item.forecast[1].low+32));var h=Math.round(9/5*(n.item.forecast[1].high+32));var p=Math.round(9/5*(n.item.forecast[1].low+32));var d=Math.round(9/5*(n.item.forecast[2].high+32));var v=Math.round(9/5*(n.item.forecast[2].low+32));var m=Math.round(9/5*(n.item.forecast[3].high+32));var g=Math.round(9/5*(n.item.forecast[3].low+32));var y=Math.round(9/5*(n.item.forecast[4].high+32));var b=Math.round(9/5*(n.item.forecast[4].low+32))}var w={title:n.item.title,temp:n.item.condition.temp,tempAlt:u,code:n.item.condition.code,todayCode:n.item.forecast[0].code,units:{temp:n.units.temperature,distance:n.units.distance,pressure:n.units.pressure,speed:n.units.speed,tempAlt:o},currently:n.item.condition.text,high:n.item.forecast[0].high,highAlt:a,low:n.item.forecast[0].low,lowAlt:f,forecast:n.item.forecast[0].text,wind:{chill:n.wind.chill,direction:i,speed:n.wind.speed},humidity:n.atmosphere.humidity,heatindex:s,pressure:n.atmosphere.pressure,rising:n.atmosphere.rising,visibility:n.atmosphere.visibility,sunrise:n.astronomy.sunrise,sunset:n.astronomy.sunset,description:n.item.description,thumbnail:"//l.yimg.com/a/i/us/nws/weather/gr/"+n.item.condition.code+"ds.png",image:"//l.yimg.com/a/i/us/nws/weather/gr/"+n.item.condition.code+"d.png",tomorrow:{high:n.item.forecast[1].high,highAlt:l,low:n.item.forecast[1].low,lowAlt:c,forecast:n.item.forecast[1].text,code:n.item.forecast[1].code,date:n.item.forecast[1].date,day:n.item.forecast[1].day,image:"//l.yimg.com/a/i/us/nws/weather/gr/"+n.item.forecast[1].code+"d.png"},forecasts:{one:{high:n.item.forecast[1].high,highAlt:h,low:n.item.forecast[1].low,lowAlt:p,forecast:n.item.forecast[1].text,code:n.item.forecast[1].code,date:n.item.forecast[1].date,day:n.item.forecast[1].day,image:"//l.yimg.com/a/i/us/nws/weather/gr/"+n.item.forecast[1].code+"d.png"},two:{high:n.item.forecast[2].high,highAlt:d,low:n.item.forecast[2].low,lowAlt:v,forecast:n.item.forecast[2].text,code:n.item.forecast[2].code,date:n.item.forecast[2].date,day:n.item.forecast[2].day,image:"//l.yimg.com/a/i/us/nws/weather/gr/"+n.item.forecast[2].code+"d.png"},three:{high:n.item.forecast[3].high,highAlt:m,low:n.item.forecast[3].low,lowAlt:g,forecast:n.item.forecast[3].text,code:n.item.forecast[3].code,date:n.item.forecast[3].date,day:n.item.forecast[3].day,image:"//l.yimg.com/a/i/us/nws/weather/gr/"+n.item.forecast[3].code+"d.png"},four:{high:n.item.forecast[4].high,highAlt:y,low:n.item.forecast[4].low,lowAlt:b,forecast:n.item.forecast[4].text,code:n.item.forecast[4].code,date:n.item.forecast[4].date,day:n.item.forecast[4].day,image:"//l.yimg.com/a/i/us/nws/weather/gr/"+n.item.forecast[4].code+"d.png"}},city:n.location.city,country:n.location.country,region:n.location.region,updated:n.item.pubDate,link:n.item.link};t.success(w)})}else{if(n.query.results===null){t.error("An invalid WOEID or location was provided.")}else{t.error("There was an error retrieving the latest weather information. Please try again.")}}});return this}})})(jQuery)
+
+
+
+
+(function($) {
+  "use strict";
+  $.extend({
+    simpleWeather: function(options){
+      options = $.extend({
+        location: '',
+        woeid: '2357536',
+        unit: 'f',
+        success: function(weather){},
+        error: function(message){}
+      }, options);
+
+      var now = new Date();
+
+      var weatherUrl = 'http://query.yahooapis.com/v1/public/yql?format=json&rnd='+now.getFullYear()+now.getMonth()+now.getDay()+now.getHours()+'&diagnostics=true&callback=?&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&q=';
+      if(options.location !== '') {
+        weatherUrl += 'select * from weather.forecast where woeid in (select woeid from geo.placefinder where text="'+options.location+'" and gflags="R") and u="'+options.unit+'"';
+      } else if(options.woeid !== '') {
+        weatherUrl += 'select * from weather.forecast where woeid='+options.woeid+' and u="'+options.unit+'"';
+      } else {
+        options.error("Could not retrieve weather due to an invalid location.");
+        return false;
+      }
+
+      $.getJSON(
+        encodeURI(weatherUrl),
+        function(data) {
+          if(data !== null && data.query.results !== null && data.query.results.channel.description !== 'Yahoo! Weather Error') {
+            $.each(data.query.results, function(i, result) {
+              if (result.constructor.toString().indexOf("Array") !== -1) {
+                result = result[0];
+              }
+
+              var compass = ['N', 'NNE', 'NE', 'ENE', 'E', 'ESE', 'SE', 'SSE', 'S', 'SSW', 'SW', 'WSW', 'W', 'WNW', 'NW', 'NNW', 'N'];
+              var windDirection = compass[Math.round(result.wind.direction / 22.5)];
+
+              if(result.item.condition.temp < 80 && result.atmosphere.humidity < 40) {
+                var heatIndex = -42.379+2.04901523*result.item.condition.temp+10.14333127*result.atmosphere.humidity-0.22475541*result.item.condition.temp*result.atmosphere.humidity-6.83783*(Math.pow(10, -3))*(Math.pow(result.item.condition.temp, 2))-5.481717*(Math.pow(10, -2))*(Math.pow(result.atmosphere.humidity, 2))+1.22874*(Math.pow(10, -3))*(Math.pow(result.item.condition.temp, 2))*result.atmosphere.humidity+8.5282*(Math.pow(10, -4))*result.item.condition.temp*(Math.pow(result.atmosphere.humidity, 2))-1.99*(Math.pow(10, -6))*(Math.pow(result.item.condition.temp, 2))*(Math.pow(result.atmosphere.humidity,2));
+              } else {
+                var heatIndex = result.item.condition.temp;
+              }
+
+              if(options.unit === "f") {
+                var unitAlt = "c";
+                var tempAlt = Math.round((5.0/9.0)*(result.item.condition.temp-32.0));
+                var highAlt = Math.round((5.0/9.0)*(result.item.forecast[0].high-32.0));
+                var lowAlt = Math.round((5.0/9.0)*(result.item.forecast[0].low-32.0));
+                var tomorrowHighAlt = Math.round((5.0/9.0)*(result.item.forecast[1].high-32.0));
+                var tomorrowLowAlt = Math.round((5.0/9.0)*(result.item.forecast[1].low-32.0));
+                var forecastOneHighAlt = Math.round((5.0/9.0)*(result.item.forecast[1].high-32.0));
+                var forecastOneLowAlt = Math.round((5.0/9.0)*(result.item.forecast[1].low-32.0));
+                var forecastTwoHighAlt = Math.round((5.0/9.0)*(result.item.forecast[2].high-32.0));
+                var forecastTwoLowAlt = Math.round((5.0/9.0)*(result.item.forecast[2].low-32.0));
+                var forecastThreeHighAlt = Math.round((5.0/9.0)*(result.item.forecast[3].high-32.0));
+                var forecastThreeLowAlt = Math.round((5.0/9.0)*(result.item.forecast[3].low-32.0));
+                var forecastFourHighAlt = Math.round((5.0/9.0)*(result.item.forecast[4].high-32.0));
+                var forecastFourLowAlt = Math.round((5.0/9.0)*(result.item.forecast[4].low-32.0));
+              } else {
+                var unitAlt = "f";
+                var tempAlt = Math.round((9.0/5.0)*result.item.condition.temp+32.0);
+                var highAlt = Math.round((9.0/5.0)*result.item.forecast[0].high+32.0);
+                var lowAlt = Math.round((9.0/5.0)*result.item.forecast[0].low+32.0);
+                var tomorrowHighAlt = Math.round((9.0/5.0)*(result.item.forecast[1].high+32.0));
+                var tomorrowLowAlt = Math.round((9.0/5.0)*(result.item.forecast[1].low+32.0));
+                var forecastOneHighAlt = Math.round((9.0/5.0)*(result.item.forecast[1].high+32.0));
+                var forecastOneLowAlt = Math.round((9.0/5.0)*(result.item.forecast[1].low+32.0));
+                var forecastTwoHighAlt = Math.round((9.0/5.0)*(result.item.forecast[2].high+32.0));
+                var forecastTwoLowAlt = Math.round((9.0/5.0)*(result.item.forecast[2].low+32.0));
+                var forecastThreeHighAlt = Math.round((9.0/5.0)*(result.item.forecast[3].high+32.0));
+                var forecastThreeLowAlt = Math.round((9.0/5.0)*(result.item.forecast[3].low+32.0));
+                var forecastFourHighAlt = Math.round((9.0/5.0)*(result.item.forecast[4].high+32.0));
+                var forecastFourLowAlt = Math.round((9.0/5.0)*(result.item.forecast[4].low+32.0));
+              }
+
+              var weather = {
+                title: result.item.title,
+                temp: result.item.condition.temp,
+                tempAlt: tempAlt,
+                code: result.item.condition.code,
+                todayCode: result.item.forecast[0].code,
+                units:{
+                  temp: result.units.temperature,
+                  distance: result.units.distance,
+                  pressure: result.units.pressure,
+                  speed: result.units.speed,
+                  tempAlt: unitAlt
+                },
+                currently: result.item.condition.text,
+                high: result.item.forecast[0].high,
+                highAlt: highAlt,
+                low: result.item.forecast[0].low,
+                lowAlt: lowAlt,
+                forecast: result.item.forecast[0].text,
+                wind:{
+                  chill: result.wind.chill,
+                  direction: windDirection,
+                  speed: result.wind.speed
+                },
+                humidity: result.atmosphere.humidity,
+                heatindex: heatIndex,
+                pressure: result.atmosphere.pressure,
+                rising: result.atmosphere.rising,
+                visibility: result.atmosphere.visibility,
+                sunrise: result.astronomy.sunrise,
+                sunset: result.astronomy.sunset,
+                description: result.item.description,
+                thumbnail: "//l.yimg.com/a/i/us/nws/weather/gr/"+result.item.condition.code+"ds.png",
+                image: "//l.yimg.com/a/i/us/nws/weather/gr/"+result.item.condition.code+"d.png",
+                tomorrow:{
+                  high: result.item.forecast[1].high,
+                  highAlt: tomorrowHighAlt,
+                  low: result.item.forecast[1].low,
+                  lowAlt: tomorrowLowAlt,
+                  forecast: result.item.forecast[1].text,
+                  code: result.item.forecast[1].code,
+                  date: result.item.forecast[1].date,
+                  day: result.item.forecast[1].day,
+                  image: "//l.yimg.com/a/i/us/nws/weather/gr/"+result.item.forecast[1].code+"d.png"
+                },
+                forecasts:{
+                  one:{
+                    high: result.item.forecast[1].high,
+                    highAlt: forecastOneHighAlt,
+                    low: result.item.forecast[1].low,
+                    lowAlt: forecastOneLowAlt,
+                    forecast: result.item.forecast[1].text,
+                    code: result.item.forecast[1].code,
+                    date: result.item.forecast[1].date,
+                    day: result.item.forecast[1].day,
+                    image: "//l.yimg.com/a/i/us/nws/weather/gr/"+result.item.forecast[1].code+"d.png"
+                  },
+                  two:{
+                    high: result.item.forecast[2].high,
+                    highAlt: forecastTwoHighAlt,
+                    low: result.item.forecast[2].low,
+                    lowAlt: forecastTwoLowAlt,
+                    forecast: result.item.forecast[2].text,
+                    code: result.item.forecast[2].code,
+                    date: result.item.forecast[2].date,
+                    day: result.item.forecast[2].day,
+                    image: "//l.yimg.com/a/i/us/nws/weather/gr/"+result.item.forecast[2].code+"d.png"
+                  },
+                  three:{
+                    high: result.item.forecast[3].high,
+                    highAlt: forecastThreeHighAlt,
+                    low: result.item.forecast[3].low,
+                    lowAlt: forecastThreeLowAlt,
+                    forecast: result.item.forecast[3].text,
+                    code: result.item.forecast[3].code,
+                    date: result.item.forecast[3].date,
+                    day: result.item.forecast[3].day,
+                    image: "//l.yimg.com/a/i/us/nws/weather/gr/"+result.item.forecast[3].code+"d.png"
+                  },
+                  four:{
+                    high: result.item.forecast[4].high,
+                    highAlt: forecastFourHighAlt,
+                    low: result.item.forecast[4].low,
+                    lowAlt: forecastFourLowAlt,
+                    forecast: result.item.forecast[4].text,
+                    code: result.item.forecast[4].code,
+                    date: result.item.forecast[4].date,
+                    day: result.item.forecast[4].day,
+                    image: "//l.yimg.com/a/i/us/nws/weather/gr/"+result.item.forecast[4].code+"d.png"
+                  },
+                },
+                city: result.location.city,
+                country: result.location.country,
+                region: result.location.region,
+                updated: result.item.pubDate,
+                link: result.item.link
+              };
+
+              options.success(weather);
+            });
+          } else {
+            if (data.query.results === null) {
+              options.error("An invalid WOEID or location was provided.");
+            } else {
+              options.error("There was an error retrieving the latest weather information. Please try again.");
+            }
+          }
+        }
+      );
+      return this;
+    }
+  });
+})(jQuery);
